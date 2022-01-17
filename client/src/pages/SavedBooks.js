0bdbbebfde6405b39e6ccaf323f673bc6ecc4243
kumikoyamagata <sun.kumiko@protonmail.com>
2022-01-17 02:02:30 -0600

    SavedBooks,SerchBook.jsreviewed

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,21 +6,26 @@ import {
   Card,
   Button
 } from "react-bootstrap";
+
 // import { getMe, deleteBook } from "../utils/mutations";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
-import { useQuery, useMutation } from "@apollo/client";
+
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
+
 const SavedBooks = () => {
   const { data } = useQuery(GET_ME);
   const userData = data?.me || {};
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
   const [removeBook] = useMutation(REMOVE_BOOK);
+
   //function to accept book mongo _id as param then deletes form db
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
+
     if (!token) {
       return false;
     }
@@ -34,6 +39,7 @@ const SavedBooks = () => {
       console.error(err);
     }
   };
+
   if (!userDataLength) {
     return <h2>LOADING...</h2>;
   }
@@ -68,6 +74,7 @@ const SavedBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className="small">Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
+                  <Card.Link href={book.link}>Link to Google Book.</Card.Link>
                   <Button
                     className="btn-block btn-danger"
                     onClick={() => handleDeleteBook(book.bookId)}
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -10,10 +10,9 @@ import {
 } from "react-bootstrap";
 
 import Auth from "../utils/auth";
-import { useQuery } from "@apollo/client";
 import { searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
-import { useMutation } from "@apollo/client";
+import { useMutation } from "@apollo/react-hooks";
 import { SAVE_BOOK } from "../utils/mutations";
 
 const SearchBooks = () => {
@@ -24,6 +23,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
   const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
@@ -48,13 +48,14 @@ const SearchBooks = () => {
       }
 
       const { items } = await response.json();
-
+      console.log(items);
       const bookData = items.map((book) => ({
         bookId: book.id,
         authors: book.volumeInfo.authors || ["No author to display"],
-        title: book.volumeInfo.title || "No title to display",
-        description: book.volumeInfo.description || "No description to display",
-        image: book.volumeInfo.imageLinks?.thumbnail || ""
+        title: book.volumeInfo.title,
+        description: book.volumeInfo.description,
+        image: book.volumeInfo.imageLinks?.thumbnail || "",
+        link: book.volumeInfo.infoLink || ""
       }));
 
       setSearchedBooks(bookData);
@@ -68,7 +69,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-    console.log(bookToSave);
+
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -80,6 +81,7 @@ const SearchBooks = () => {
       await saveBook({
         variables: { ...bookToSave, token }
       });
+
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
       console.log("Book Saved!");
@@ -136,6 +138,7 @@ const SearchBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className="small">Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
+                  <Card.Link href={book.link}>Link to Google Book.</Card.Link>
                   {Auth.loggedIn() && (
                     <Button
                       disabled={savedBookIds?.some(
